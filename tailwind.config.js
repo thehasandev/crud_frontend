dfaa30be3ddc828f10e23ef76368bf6eace10fc2
Hasan <thehasandev@gmail.com>
2024-02-14 22:14:02 +0600

    add crud from

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,14 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary': '#32375C',
+        'secondary': '#222222',
+        'gray': '#D3D3D3',
+        'offwhite': '#7A7A7A',
+      },
+    },
   },
   plugins: [],
 }
