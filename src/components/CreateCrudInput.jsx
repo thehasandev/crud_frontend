1d9a1cfcac16d00fd5ecafb2c52f2339ef40d55f
Hasan <thehasandev@gmail.com>
2024-02-14 22:21:29 +0600

    add submit funtion

--- src/components/CreateCrudInput.jsx
+++ src/components/CreateCrudInput.jsx
@@ -3,6 +3,9 @@ import React, { useState } from 'react'
 function CreateCrudInput() {
     const [name,setName] = useState("")
     const [des,setDes] = useState("")
+    const handleSubmit =()=>{
+        console.log(des,name);
+    }
   return (
     <div>
            <div className='flex flex-col-reverse md:flex-row gap-y-5 justify-center items-center py-8 md:py-0'>
@@ -10,18 +13,18 @@ function CreateCrudInput() {
                 <div className='w-full md:pl-10'>
                     <div className='mt-6'>
                         <p className='font-inter font-bold text-base text-secondary mb-2'>Name</p>
-                        <input onChange={(e)=setName(e.target.value)} type="text" name='userName' placeholder='Name here' className='w-[300px] md:w-10/12 border border-gray p-3.5 rounded-[2px]' />
+                        <input onChange={(e)=>setName(e.target.value)} type="text" name='userName' placeholder='Name here' className='w-[300px] md:w-10/12 border border-gray p-3.5 rounded-[2px]' />
                     </div>
 
 
                     <div className='mt-6'>
                         <p className='font-inter font-bold text-base text-secondary mb-2'>Description</p>
-                        <textarea onChange={(e)=setDes(e.target.value)} type="text" name='userEmail' placeholder='Enter your Description' className='w-[300px] md:w-10/12 border border-gray p-3.5 rounded-[2px]' />
+                        <textarea onChange={(e)=>setDes(e.target.value)} type="text" name='userEmail' placeholder='Enter your Description' className='w-[300px] md:w-10/12 border border-gray p-3.5 rounded-[2px]' />
                     </div>
 
 
                     <div className="flexjustify-center md:justify-start">
-                        <button className=' px-10 rounded-[2px] bg-black/80 py-3 text-white font-inter font-semibold text-lg mt-7'>Submit</button>
+                        <button onClick={handleSubmit} className=' px-10 rounded-[2px] bg-black/80 py-3 text-white font-inter font-semibold text-lg mt-7'>Submit</button>
                     </div>
 
                 </div>
